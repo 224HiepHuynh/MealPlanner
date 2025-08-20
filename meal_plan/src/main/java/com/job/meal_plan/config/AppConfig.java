fdb0308db09a1de44b62dd80980d503d7492dc4b
OhMyHiep <huynhhc@lemoyne.edu>
2025-08-20 02:06:49 -0400

    got it to read yml file for datasource rather than hard coded data

--- meal_plan/src/main/java/com/job/meal_plan/config/AppConfig.java
+++ meal_plan/src/main/java/com/job/meal_plan/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.job.meal_plan.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public DataSource dataSource() {
+        @Bean
+    public DataSource dataSource(
+        @Value("${spring.datasource.url}") String url,
+        @Value("${spring.datasource.driver-class-name}") String driverClassName,
+        @Value("${spring.datasource.username}") String username,
+        @Value("${spring.datasource.password}") String password) {
+
         return DataSourceBuilder.create()
-                .driverClassName("org.h2.Driver")
-                .url("jdbc:h2:mem:mealplan_db")
-                .username("sa")
-                .password("")
+                .url(url)
+                .driverClassName(driverClassName)
+                .username(username)
+                .password(password)
                 .build();
     }
 }
